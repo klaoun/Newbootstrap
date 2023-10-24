c16165f058966816202b8d863d012b35e0a49b1f
klaoun <36087587+klaoun@users.noreply.github.com>
2023-10-24 02:52:19 +0200

    Update themeconf.inc.php

--- themeconf.inc.php
+++ themeconf.inc.php
@@ -7,11 +7,11 @@ Theme URI:
 Author:
 Author URI:
 */
-require_once(PHPWG_THEMES_PATH . 'bootstrap_darkroom/include/themecontroller.php');
-require_once(PHPWG_THEMES_PATH . 'bootstrap_darkroom/include/config.php');
+require_once(PHPWG_THEMES_PATH . 'newbootstrap_darkroom/include/themecontroller.php');
+require_once(PHPWG_THEMES_PATH . 'newbootstrap_darkroom/include/config.php');
 
 $themeconf = array(
-    'name' => 'Newbootstrap_darkroom',
+    'name' => 'newbootstrap_darkroom',
     'parent' => 'default',
     'load_parent_css' => false,
     'load_parent_local_head' => true,
@@ -28,5 +28,5 @@ pwg_set_session_var('show_metadata', true);
 $video_ext = array('mp4','m4v');
 $conf['file_ext'] = array_merge ($conf['file_ext'], $video_ext, array_map('strtoupper', $video_ext));
 
-$controller = new \BootstrapDarkroom\ThemeController();
+$controller = new \newBootstrapDarkroom\ThemeController();
 $controller->init();
